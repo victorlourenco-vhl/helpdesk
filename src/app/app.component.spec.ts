7d8e1a5cb84bf21222257c4b3524163537848899
Victor Lourenço <victorhlo12@hotmail.com>
2024-05-18 13:24:33 -0300

    CONFIG: Adicionando o Angular Material e excluindo o HTML inicial do projeto

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -8,22 +8,5 @@ describe('AppComponent', () => {
     declarations: [AppComponent]
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'helpdesk'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('helpdesk');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('helpdesk app is running!');
-  });
+  
 });
