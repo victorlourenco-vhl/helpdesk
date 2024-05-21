cc1c724dd3e271d0e086c9a550da5db43a64c1b7
Victor Lourenço <victorhlo12@hotmail.com>
2024-05-21 15:16:50 -0300

    FEAT: Criando o componente nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './components/nav/nav.component';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
     standalone: true,
-    imports: [RouterOutlet]
+    imports: [RouterOutlet, NavComponent]
 })
 export class AppComponent {
   title = 'helpdesk';
