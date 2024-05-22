4a6c5918d5b5c8c38a7001e0f87a7f3d21e46897
Victor Lourenço <victorhlo12@hotmail.com>
2024-05-22 15:13:57 -0300

    FEAT: Criando o menu lateral com angular material

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatListModule, MatSidenavModule, MatButtonModule],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrl: './nav.component.css',
+  encapsulation: ViewEncapsulation.None
 })
 export class NavComponent {
-
+  showFiller = false;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,14 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
 import { importProvidersFrom } from '@angular/core';
-import { AppComponent } from './app/app.component';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app/app-routing.module';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
 
 
 bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(BrowserModule, AppRoutingModule),
-        provideAnimations()
-    ]
+  providers: [
+    importProvidersFrom(BrowserModule, AppRoutingModule),
+    provideAnimations()
+  ]
 })
   .catch(err => console.error(err));
