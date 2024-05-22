8bd2c91d8c2c280da4b39c58073535fa3d53b95b
Victor Lourenço <victorhlo12@hotmail.com>
2024-05-22 18:24:18 -0300

    FEAT: Criando o componente home

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,17 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { RouterOutlet } from '@angular/router';
+import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [MatIconModule, MatListModule, MatSidenavModule, MatButtonModule],
+  imports: [MatIconModule, MatListModule, MatSidenavModule, MatButtonModule, RouterOutlet],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
-export class NavComponent {
+export class NavComponent implements OnInit{
   showFiller = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.navigate(['home'])
+  }
 }
