f02245f684b0a2de335755124152d9a178e06aee
Victor Lourenço <victorhlo12@hotmail.com>
2024-05-24 09:50:17 -0300

    CONFIG: Implementado rota para login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { HomeComponent } from './components/home/home.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  
   {
     path: '', component: NavigationComponent, children: [
-      {path: 'home', component: HomeComponent},
-      {path: 'tecnicos', component: TecnicoListComponent}
+      { path: 'home', component: HomeComponent },
+      { path: 'tecnicos', component: TecnicoListComponent }
     ]
   },
-  {path: '**', component: NavigationComponent}
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: NavigationComponent }
 ];
 
 @NgModule({
