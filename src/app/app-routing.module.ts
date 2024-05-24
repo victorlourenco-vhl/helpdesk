5714ecb2f4b7992a31a1a56fb548e2299fda1007
Victor Lourenço <victorhlo12@hotmail.com>
2024-05-24 09:49:03 -0300

    FEAT: Criando o componente de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './components/home/home.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 
 const routes: Routes = [
+  
   {
     path: '', component: NavigationComponent, children: [
       {path: 'home', component: HomeComponent},
